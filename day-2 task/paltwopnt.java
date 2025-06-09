9374eaf098cc26532ee48789e90abf2e60276c58
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-09 09:15:45 +0530

    day-2 task completed

---
+++ day-2 task/paltwopnt.java
@@ -0,0 +1,24 @@
+package dsa_java;
+
+class paltwopnt {
+    public static void main(String[] args) {
+        String a="madam";
+        int i=0;
+        int j=a.length() -1;
+        boolean pallindrome=true;
+        while(i<j){
+        if (a.charAt(i)!=a.charAt(j)){
+            pallindrome=false;
+            break;
+        }
+        i++;
+        j--;
+        }
+        if(pallindrome==true){
+            System.out.println("is pallindrome");
+        }
+        else{
+             System.out.println("is not pallindrome");
+        }
+    }
+}
